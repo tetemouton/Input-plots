ec391d9d9a95b214581c9670d9196c014c4a4168
tetemouton <samm@spc.int>
2016-03-16 19:49:32 +1100

    added fisheries summaries

---
+++ Plot_Fisheries_Summaries.r
@@ -0,0 +1,140 @@
+library(splines)
+library(scales)
+library(ggplot2)
+library(MASS)
+library(reshape2)
+library(plyr)
+library(grid)
+library(ggmap)
+library(maps)
+library(mapplots)
+library(mapproj)
+library(mapdata)
+library(rgl)
+
+# Set the drive that is being worked off, if on my compter work off the C:/ else work off penguin
+    drv <- ifelse(Sys.info()["nodename"] == "SPC122114", "C:/skj/2016", "//penguin/assessments/skj/2016")
+
+    setwd(paste0(drv, "/assessment/Data_preperation/Fisheries_Structure"))
+    source("support.functions.r")
+    
+    theme_set(theme_bw())
+    
+    setInternet2(TRUE) # Sometimes permissions prevent generating the map unless this is set
+
+# Get a map to be used for plotting the extent of the individual fisheries
+    reg.map <- get_map(location = c(160,15), zoom = 3, maptype = 'satellite')# maptype = 'roadmap')
+
+# Need to define the subregions for each fishery unfortunately - will try to automate via extraction of regions from muffie at some stage
+    reg_defs <- read.csv("Assessment_Region_Coordinates_SKJ.csv", header=TRUE, stringsAsFactors=FALSE)
+#    reg_defs <- read.csv("Assessment_Region_Coordinates_SKJ_alt.csv", header=TRUE, stringsAsFactors=FALSE)
+
+    regKeep <- reg_defs$MufArea 
+
+# List below explicitely links the 33 fisheries to the regions they are in. Note some in R7 don't cover the whole region so have to state thier subregions
+    #fsh.dat <- data.frame(reg = c(1,1,1,2,2,2,2,5,5,5,5,3,3,3,3,4,4,4,4,4,4,4,4))
+    fsh.reg = c(1,1,1,2,2,2,2,5,5,5,5,3,3,3,3,4,4,4,4,4,4,4,4)
+#    fsh.reg = c(1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4,NA,4,5,5,5,5,6,6,6,6,6,7,7,7,4,4)
+
+
+    regHigh <- lapply(1:length(fsh.reg), function(x) reg_defs$MufArea[fsh.reg[x] == reg_defs$reg])
+
+
+# Exclusion lists
+# There are some cases where size data exists for a fleet that does not have catch in that fishery - might be a scale problem, data entry etc. etc. - all very minor so removed
+#     alb.exc = list(F1 = 'NONE', F2 = 'NONE', F3 = 'NONE', F4 = 'NONE', F5 = 'NONE', F6 = 'NONE', F7 = 'NONE', F8 = 'NONE', F9 = 'NONE', F10 = 'NONE',
+#                    F11 = 'NONE', F12 = 'NONE', F13 = 'NONE', F14 = 'NONE', F15 = 'NONE', F16 = 'NONE', F17 = 'NONE', F18 = 'NONE', F19 = 'NONE', F20 = 'NONE',
+#                    F21 = 'NONE', F22 = 'NONE', F23 = 'NONE', F24 = 'NONE', F25 = 'NONE', F26 = 'NONE', F27 = 'NONE', F28 = 'NONE', F29 = 'NONE', F30 = 'NONE',
+#                    F31 = 'NONE', F32 = 'NONE', F33 = 'NONE')
+
+    fsh.exc <- as.list(rep("NONE", length(fsh.reg)))
+
+# Define a couple of species specific parameters
+    mod="SKJ_1_detail"
+#    mod="SKJ_3_detail"
+    fisheries=1:length(fsh.reg)
+    Lrange=c(2,110)
+    fyr=1975
+    lyr=2015
+
+# Structure all the data
+    cfile = paste(mod, "_CE.csv", sep='') # Defines the name of the file containing the catch data for the different fisheries
+    lfile = paste(mod, "_LF.csv", sep='') # Defines the name of the file containing the length data for the different fisheries
+
+    Cdat = read.csv(cfile, header=TRUE)          # load catch data
+    Cdat = rename(Cdat, c("yy"="yr"))        # rename variables to make things easier for functions later on
+    Cdat = ExpandDat(dat=Cdat, year.lab="yr", fisheries=fisheries, first.yr=fyr, last.yr=lyr)    # calls function that cleans and reshapes the data
+
+    Ldat = read.csv(lfile, header=TRUE)                                         # Loads length data
+    Ldat = ExpandDat(dat=Ldat, year.lab="yr", fisheries=fisheries, first.yr=fyr, last.yr=lyr)   # calls function that cleans and reshapes the data
+    Ldat$len = as.numeric(as.character(Ldat$len))
+    Ldat = Ldat[Ldat$len >= Lrange[1] & Ldat$len < Lrange[2],]                           # culls data outside size range used in assessment
+    Ldat = rename(Ldat, c('lf_samples' = 'freq'))
+    L.avail = ifelse(is.na(match(fisheries, Ldat$fsh))=='TRUE',0,1)     # creates variable which indicates the availability of data, prevents crashing if there's no data
+
+# Defines some colours for the different fleets
+  # The list below started as just a few fleets for LL, but increased and increased - should have just chosen a palette in retrospect...
+  # This list sets the colours for the shitload of fleets in the dataset, if two major fleets are too similar delete one between the two fleets and it will change one of them
+    fullist = c('#FFCC00', '#330099', '#CCFF99', '#000000', '#FF00CC', '#FFFF00', '#FFFFCC', '#99CC99', '#33FF00', '#FF6600', '#3366FF', '#663333', '#6699FF', '#9966CC',
+                '#990000', '#CCFFCC', '#003200', '#CCFFFF', '#3333FF', '#666666', '#FF6600', '#99FF33', '#CC00FF', '#660000', '#FF6699', '#3300CC', '#00CCFF', '#00FF99', '#CCCC33', '#FFFF66',
+                '#9999FF', '#330000', '#6633FF', '#FF99FF', '#0066FF', '#6600FF', '#333366', '#99FF00', '#33FF66', '#FF9933', '#CCCC00', '#FF00FF', '#FF0066', '#993300', '#999999', '#333333',
+                '#CC0000', '#FF3366', '#CCCC66', '#CCCC99', '#669933', '#3399FF', '#99CCFF', '#00CC66', '#009966', '#33FF99', '#0099FF', '#0033FF', '#CC9966', '#CC6600', '#CC9999', '#CCFF33', '#FF9999', '#FFCCCC',
+                '#9933FF', '#FFFF33', '#FFFF99')
+    collist = fullist[1:length(unique(c(Cdat$FlgFlt,Ldat$FlgFlt)))]   # Pulls out the actual number of colours needed based on the fleets in all the data
+    names(collist) = sort(unique(c(Cdat$FlgFlt,Ldat$FlgFlt)))         # Gives the colours names so that the fleets can be matched to a colour and the colour can be kept consistent between plots
+
+# The actual function that does it all and produces the plots
+    generate.fsh.plot = function(fishery=1, prelim.filenm="//penguin/assessments/skj/2016/assessment/Data_preperation/Fisheries_Structure/5Reg/FshPlot_F")
+    {
+        Fsh.plot <- plot.fishery(reg.keep=regKeep, reg.highlight=regHigh, fishy=fishery, reg_defs=reg_defs)   # makes the map of the fishery
+        
+        C.plot <- Catch.Plot(dat=Cdat, fishery=fishery, collist=collist, all.yrs = 1970:2013, brwidth = 1)                              # makes the plot of the number or weight of the fish caught
+        
+        if(L.avail[fishery] == 1)   # if there are length samples then plot the number of samples and the median size of the samples
+        {
+          L.N.tab <- create.N.tab(dat=Ldat, fishery=fishery, exc.list=fsh.exc[[fishery]], all.yrs=seq(1970,2015,1), by_yr=TRUE)
+          L.N.plot <- Sz.N.Plot(tab.dat=L.N.tab, y.lab="No. fish measured", collist=collist, xbrks=seq(1970, 2010, 10), brwidth = 1)
+    
+          L.Med.tab <- create.Med.tab(dat=Ldat, sz.lab="len", fishery=fishery, exc.list=fsh.exc[[fishery]], all.yrs=seq(1970,2015,1), by_yr=TRUE)
+          L.Med.plot <- Sz.Med.Plot(tmp.med=L.Med.tab, y.lab="Median length (cm)", collist=collist)
+          
+          size.plt <- size.dist.plot(dat=Ldat, sz.lab="len", fishery=fishery, exc.list=fsh.exc[[fishery]], all.yrs = seq(1970, 2010, 10), by_yr=TRUE, rnding=2) 
+        }
+    
+    # Set up full plot    
+        windows(2000,1400)
+    
+            pushViewport(viewport(layout = grid.layout(3,2)))   # make a grid of 2 rows by 2 columns
+            print(C.plot, vp = viewport(layout.pos.row=1, layout.pos.col=1))   # put the catch plot in row 1 column 1 etc. etc.
+            print(Fsh.plot, vp = viewport(layout.pos.row=1, layout.pos.col=2))
+            
+            if(L.avail[fishery] == 1)
+            {
+              print(L.N.plot, vp = viewport(layout.pos.row=2, layout.pos.col=1))
+              print(L.Med.plot, vp = viewport(layout.pos.row=2, layout.pos.col=2))
+            }
+    
+            print(size.plt, vp=viewport(layout.pos.row=3, layout.pos.col=1:2))
+        
+        savePlot(paste0(prelim.filenm, fishery),type='png')
+        dev.off()
+    }
+    
+    
+    sil <- lapply(1:2, generate.fsh.plot)
+
+
+
+# Alternative plots to check the JP only P fishery in R2 assumption
+    for(i in 1:length(fsh.reg)) generage.fsh.plot(i, prelim.filenm="//penguin/assessments/skj/2016/assessment/Data_preperation/Fisheries_Structure/5Reg_Alt/FshPlot_F")
+
+
+# Alternative spatial structure plots
+    for(i in 27:32) generate.fsh.plot(i, prelim.filenm=paste0(drv, "/assessment/Data_preperation/Fisheries_Structure/5Reg_Alt/FshPlot_F"))
+
+    
+
+
+
+
+
---
+++ support.functions.r
@@ -0,0 +1,206 @@
+
+ExpandDat = function(dat=cdat, year.lab="yr", fisheries=1:30, first.yr=1960, last.yr=2013)   # year.lab now effectively redundant but left it in out of laziness
+{
+    dat <- dat[dat$fishery_no %in% fisheries,]                                   # Excludes data not in fisheries
+    dat <- dat[dat[,year.lab] >= first.yr & dat[,year.lab] <= last.yr,]               # Excludes data before 1960
+    dat$yrqtr <- dat[,year.lab] + dat$qq/4 - 0.125
+    dat$FlgFlt <- paste(dat$flag_id, dat$fleet_id)
+    rename(dat, c("fishery_no" = "fsh"))
+}
+
+create.N.tab = function(dat=Ldat, fishery=1, exc.list=exc.list, all.yrs=seq(1960.125,2013.875,0.25), by_yr=FALSE)   # Gets data into the right form to produce the number of samples plots
+{                                               # makes sure that if there is no data in a certain year then there is an NA and hence ggplot plots a zero
+    tmp.dat <- dat[dat$fsh == fishery,]
+    tmp.dat <- tmp.dat[!(tmp.dat$FlgFlt %in% exc.list),]   # Excludes size samples for fleets where there are size samples but no catch - decides which to exclude based on the exc.list list in the working script
+    
+    if(by_yr)
+    {
+        tab.dat <- tapply(tmp.dat$freq, list(tmp.dat$yr,tmp.dat$FlgFlt), sum)  
+    } else {
+        tab.dat <- tapply(tmp.dat$freq, list(tmp.dat$yrqtr,tmp.dat$FlgFlt), sum)      
+    }
+    
+    yr.match <- match(all.yrs, rownames(tab.dat))
+    tab.dat <- data.frame("yr"=as.character(all.yrs), tab.dat[yr.match,],row.names=NULL); names(tab.dat) = c("yr",sort(unique(tmp.dat$FlgFlt)))
+    tab.dat <- melt(tab.dat, id.vars="yr", measure.vars=dimnames(tab.dat)[[2]][-1]); names(tab.dat) = c("yr","Fleet","Nsamples")  # Put into long format for plot
+              
+    return(tab.dat)
+}
+
+
+create.Med.tab = function(dat=Ldat, sz.lab="len", fishery=1, exc.list=exc.list, all.yrs = seq(1960.125,2013.875,0.25), by_yr=FALSE)   # Calculates median size for each year there is data and puts into format that can be plotted
+{
+    tmp.dat <- dat[dat$fsh == fishery,]
+    tmp.dat <- tmp.dat[!(tmp.dat$FlgFlt %in% exc.list),]   # Excludes size samples for fleets where there are size samples but no catch - decides which to exclude based on the exc.list list in the working script
+    
+    if(by_yr)
+    {
+      tab.dat <- aggregate(tmp.dat$freq, by=list(yr = tmp.dat[,"yr"], Fleet = tmp.dat[,"FlgFlt"], Size = tmp.dat[,sz.lab]), FUN=sum) # aggregates number of samples at each size by year and fleet  
+    } else {
+      tab.dat <- aggregate(tmp.dat$freq, by=list(yr = tmp.dat[,"yrqtr"], Fleet = tmp.dat[,"FlgFlt"], Size = tmp.dat[,sz.lab]), FUN=sum) # aggregates number of samples at each size by year and fleet     
+    }        
+    
+    tab.dat$ind <- 1:dim(tab.dat)[1]
+
+    new.ind <- rep(tab.dat$ind,tab.dat$x)   # Makes a variable that indicates how many samples there are for each size in that year/fleet
+    new.dat <- tab.dat[match(new.ind, tab.dat$ind),]   # Expands the dataset from tab.dat which is aggregated by length to a full dataset where each row is an individual fish
+
+    tmp.med <- tapply(new.dat[,"Size"], list(new.dat[,"yr"], new.dat[,"Fleet"]), FUN=function(x) quantile(x, 0.5))   # Now take the median over the expanded data
+    yr.match <- match(all.yrs, rownames(tmp.med))   # Make sure there is a median or an NA for each year
+    tmp.med <- data.frame("yr"=as.character(all.yrs), tmp.med[yr.match,],row.names=NULL); names(tmp.med) = c("yr",sort(unique(tmp.dat$FlgFlt)))
+    tmp.med <- melt(tmp.med, id.vars="yr", measure.vars=dimnames(tmp.med)[[2]][-1]); names(tmp.med) = c("yr","Fleet","Size")   # put into long format for plotting
+      
+    return(tmp.med)
+}
+
+
+
+Catch.Plot = function(dat=dat, fishery=1, collist=collist, all.yrs = 1970:2013, brwidth = 0.9)   # Calculates and plots catch of a fishery by year and fleet
+{
+    tmp.dat <- dat[dat$fsh == fishery,]    
+
+    if(tmp.dat[1,"gr_id"] %in% c("L","T"))   # If the gear of the fishery is longline or troll then calculate catch in numbers of fish (sp_n)
+    {
+        tab.dat <- tapply(tmp.dat$sp_n, list(tmp.dat$yr,tmp.dat$FlgFlt), FUN=sum)
+    } else {                        # If the gear of the fishery is something else then calculate catch in weight (sp_c)
+        tab.dat <- tapply(tmp.dat$sp_c, list(tmp.dat$yr,tmp.dat$FlgFlt), FUN=sum)
+    }
+    
+    yr.match <- match(all.yrs, rownames(tab.dat))
+      tab.dat <- data.frame("yr"=as.character(all.yrs), tab.dat[yr.match,],row.names=NULL); names(tab.dat) = c("yr",sort(unique(tmp.dat$FlgFlt)))
+        tab.dat <- melt(tab.dat, id.vars="yr", measure.vars=dimnames(tab.dat)[[2]][-1]); names(tab.dat) = c("yr","Fleet","Nsamples")  # put into long format for plotting
+          Ncol <- ifelse(length(unique(tab.dat$Fleet)) > 16, 2, 1)   # This determines how many columns the legend is - if there are heaps of fleets (e.g. some ps fisheries) then the legend is 2 columns, otherwise just one
+    
+      p <- ggplot(data=tab.dat, aes(x=yr, y=Nsamples/1000, fill=Fleet)) +   # Divide by 1000 to keep the y scale managable
+                  geom_bar(stat="identity", colour="black", width=brwidth) +
+                  scale_fill_manual(name="Fleet", values=collist) 
+          if(tmp.dat[1,"gr_id"] == "L")
+          {
+              p <- p + xlab("Year") + ylab("Catch (fish x 1000)")   # If longline fishery axis label in no. of fish
+          } else {
+              p <- p + xlab("Year") + ylab("Catch (MT x 1000)")     # If something else then axis label in metric tonnes
+          }
+
+          p <- p + scale_x_discrete(breaks=pretty_breaks(n=5)) + # manually insert breaks and labels for consistency across fisheries
+                   theme(legend.key.size = unit(0.08,"cm"), legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0,1),
+                         legend.background = element_rect(fill="transparent"), panel.grid.major=element_blank(),
+                         panel.grid.minor=element_blank()) +  # make legend unit size small else it takes up too much room
+                   ggtitle(paste("Fishery",fishery)) +        # plot the fishery name as the title - in future might be nice to give the actual fishery name e.g. all_flag_3 etc.
+                   guides(fill=guide_legend(ncol=Ncol))       # insert legend for fleets, and determine columns from Ncol
+      p
+}
+
+
+
+Sz.N.Plot <- function(tab.dat=L.N.tab, y.lab="No. fish measured", collist=collist, xbrks=seq(1970, 2010, 10), brwidth = 0.9)   # Makes a plot of the number of size samples for a fishery - bar plot
+{
+    tab.dat$yr <- as.character(tab.dat$yr)
+
+        p <- ggplot(data=tab.dat, aes(x=yr, y=Nsamples, fill=Fleet)) +
+                    geom_bar(stat="identity", colour="black", width=brwidth) +
+                    scale_fill_manual(name="Fleet", values=collist) +
+                    xlab("Year") + ylab(y.lab) +
+                    scale_x_discrete(breaks=xbrks, labels=xbrks) +
+                    theme(legend.key.size = unit(0.08,"cm"), legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0,1),
+                          legend.background = element_rect(fill="transparent"), panel.grid.major=element_blank(),
+                          panel.grid.minor=element_blank())
+        p
+}
+
+Sz.Med.Plot <- function(tmp.med=L.Med.tab, y.lab="Median length (cm)", collist=collist)   # Makes a plot of the median sizes for a fishery - line plot
+{
+    tmp.med$yr <- as.numeric(as.character(tmp.med$yr))
+
+        p <- ggplot(data=tmp.med, aes(x=yr, y=Size, colour=Fleet)) +
+                    geom_point() +
+                    geom_line() +
+                    scale_colour_manual(name="Fleet", values=collist) +
+                    xlab("Year") + ylab(y.lab) +
+                    scale_x_continuous(breaks=pretty_breaks(n=5)) +
+                    theme(legend.key.size = unit(0.08,"cm"), legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0,1),
+                          legend.background = element_rect(fill="transparent"), panel.grid.major=element_blank(),
+                          panel.grid.minor=element_blank(), legend.key=element_rect(colour="transparent"))
+        p
+}
+
+# This function plots the map of the spatial extent of the individual fisheries
+plot.fishery = function(reg.keep=reg.keep, reg.highlight=reg.highlight, fishy=1, reg_defs=reg_defs) # Remove the getmap step and things will speed up immensely
+{
+
+   reg.bounds.x <- cbind(reg_defs$lon1n, reg_defs$lon2n, reg_defs$lon2n, reg_defs$lon1n, reg_defs$lon1n) 
+   reg.bounds.y <- cbind(reg_defs$lat1n, reg_defs$lat1n, reg_defs$lat2n, reg_defs$lat2n, reg_defs$lat1n) 
+  
+      p <- ggmap(reg.map, fullpage=TRUE)  # pull in the map (reg.map) that was extracted in the working script, you could put the get map step in here if different base maps were needed for different fisheries
+      #p = ggmap(reg.map)
+
+      for(i in 1:length(reg.highlight[[fishy]]))   # this loop determines which region/s or subregion/s are highlighted - appear as a shaded polygon - determined by the reg.highlight list in the working script
+      {
+          ind <- match(reg.highlight[[fishy]][i], reg_defs$MufArea)
+          dat <- data.frame(x=reg.bounds.x[ind,], y=reg.bounds.y[ind,])  # matches the highlight list with the bounds given above to determine the polygon to be plotted
+          p <- p + geom_polygon(data=dat, mapping=aes(x=x, y=y), fill=alpha("red", 0.5))   # plot a semi-transparent polygon
+          
+          if(i == 1)
+          {
+              txtdat <- data.frame(x=mean(c(reg_defs[ind,"lon1n"],reg_defs[ind,"lon2n"])),
+                                   y=mean(c(reg_defs[ind,"lat1n"],reg_defs[ind,"lat2n"])),
+                                   r=reg_defs[ind,"reg"])  
+              p <- p + geom_text(data=txtdat, aes(x=x, y=y, label=r, colour="white"), hjust=0, vjust=1, size=5)
+          }
+      }
+      
+      for(i in 1:length(reg.keep))   # this loop determines which regions/subregions are plotted as outlines - based on a list of regions to "keep" and their bounds
+      {
+          dat <- data.frame(x=reg.bounds.x[i,],y=reg.bounds.y[i,])
+          #dat.txt <- reg.txt[reg.txt$reg == i,]
+          p <- p + geom_polygon(data=dat, mapping=aes(x=x, y=y), fill=alpha("red", 0.25))
+          #p <- p + geom_text(data=dat.txt, aes(x=x, y=y, label=r, colour="red"), hjust=0, vjust=0, size=3)
+      }
+
+      p <- p + theme(legend.position="none")
+      p
+}
+
+
+size.dist.plot = function(dat=Ldat, sz.lab="len", fishery=1, exc.list=exc.list, all.yrs = seq(1960.125,2013.875,0.25), by_yr=FALSE, rnding=2)   # Calculates median size for each year there is data and puts into format that can be plotted
+{
+  
+  tmp.dat <- dat[dat$fsh == fishery,]
+  tmp.dat <- tmp.dat[!(tmp.dat$FlgFlt %in% exc.list),]   # Excludes size samples for fleets where there are size samples but no catch - decides which to exclude based on the exc.list list in the working script
+  
+  if(by_yr)
+  {
+    tab.dat <- aggregate(tmp.dat$freq, by=list(yr = tmp.dat[,"yr"], Fleet = tmp.dat[,"FlgFlt"], Size = tmp.dat[,sz.lab]), FUN=sum) # aggregates number of samples at each size by year and fleet  
+  } else {
+    tab.dat <- aggregate(tmp.dat$freq, by=list(yr = tmp.dat[,"yrqtr"], Fleet = tmp.dat[,"FlgFlt"], Size = tmp.dat[,sz.lab]), FUN=sum) # aggregates number of samples at each size by year and fleet     
+  }        
+  
+  tab.dat$ind <- 1:dim(tab.dat)[1]
+  
+  new.ind <- rep(tab.dat$ind,tab.dat$x)   # Makes a variable that indicates how many samples there are for each size in that year/fleet
+  new.dat <- tab.dat[match(new.ind, tab.dat$ind),]   # Expands the dataset from tab.dat which is aggregated by length to a full dataset where each row is an individual fish
+  
+  #new.dat$JSize <- jitter(new.dat$Size, 3)
+  new.dat$RSize <- floor(new.dat$Size/rnding)*rnding
+  
+  p <- ggplot(data=new.dat, aes(x=yr, y=RSize, group=yr)) +
+              # geom_jitter(height=0, colour=alpha("black",0.002)) +
+              geom_violin(colour="transparent", fill=alpha("black",0.5), scale="width") +
+              xlab("Year") + ylab("Length (cm)") + xlim(fyr-2, lyr) +
+              scale_x_continuous(breaks=pretty_breaks(n=5)) +
+              theme(legend.key.size = unit(0.08,"cm"), legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0,1),
+                    legend.background = element_rect(fill="transparent"), panel.grid.major=element_blank(),
+                    panel.grid.minor=element_blank(), legend.key=element_rect(colour="transparent"))
+  
+  p <- ggplot(data=new.dat, aes(x=yr, y=RSize)) + geom_tile(aes(fill = Pfish)) +
+              xlab("Year") + ylab("Length (cm)") + xlim(fyr-2, lyr) +
+              scale_x_continuous(breaks=pretty_breaks(n=5)) +
+              theme(legend.key.size = unit(0.08,"cm"), legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0,1),
+                    legend.background = element_rect(fill="transparent"), panel.grid.major=element_blank(),
+                    panel.grid.minor=element_blank(), legend.key=element_rect(colour="transparent"))
+  
+  
+  p
+}
+
+
+
